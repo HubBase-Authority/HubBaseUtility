0ec3451e48d37749e72de3a288998a469f2b490c
Voice659 <E.R.Fahretdinov@yandex.ru>
2026-06-11 18:55:10 +0300

    HBUtil v0.0.0.0.20 is out!
    Signed-off-by: Voice659 <E.R.Fahretdinov@yandex.ru>

--- src/HubBaseUtility/Main.py
+++ src/HubBaseUtility/Main.py
@@ -22,18 +22,22 @@ def Programm2():
     pass
 
 
-__version__ = "0.0.0.0.11"
+__version__ = "0.0.0.0.20"
 ProgrammNumber = 1
 programmList = {1: Programm1}
 
 
 def Showcase():
     print(f"HubBase Utility {__version__} programm showcase - {ProgrammNumber} programms")
+    ProgrammCycle(ProgrammNumber, programmList, time.sleep, [1])
+
+
+def ProgrammCycle(ProgrammNumber: int, programmList: dict, TransitionMethod, TransitionMethodargs: list):
     for programm in range(1, ProgrammNumber + 1):
         print(f"Programm №{programm} launching")
-        time.sleep(1)
         try:
             programmList[programm]()
+            TransitionMethod(*TransitionMethodargs)
         except KeyError:
             print(f"KeyError: Key {programm} is out of reach")
             break
